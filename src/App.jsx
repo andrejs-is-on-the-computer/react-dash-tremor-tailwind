6f9d8e3398039f5ba62e59f1ec7b0c76d6e64d73
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-10-19 17:30:18 +0200

    2819 sidebar navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
       <Sidebar />
       <div className='flex flex-col flex-1 relative'>
         <Navbar />
+        <div className='grid md:grid-cols-3 grid-cols-1 w-full'>
+          <div className='col-span-2'>Left Column</div>
+          <div className='w-full'>Right Column</div>
+        </div>
       </div>
     </main>
   )
