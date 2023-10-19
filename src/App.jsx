103d9824b0429b217bf2b0cd8324061ab700ce89
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-10-19 17:24:04 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <main className='flex'>
+      <Sidebar />
+      <div className='flex flex-col flex-1 relative'>
+        <Navbar />
+      </div>
+    </main>
+  )
+}
+
+export default App
