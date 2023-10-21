3c6b9d60d3b9d3aa5884ba01ae4bc5d757e50cf0
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-10-21 12:09:17 +0200

    4444 donut chart

---
+++ src/components/SalesItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    BadgeDelta,
+    Button,
+    Card,
+    DonutChart,
+    Flex,
+    TabGroup,
+    Tab,
+    TabList,
+    Bold,
+    Divider,
+    List,
+    ListItem,
+    Metric,
+    Text,
+    Title,
+} from '@tremor/react';
+
+const SalesItem = () => {
+    return (
+        <div className="w-full p-2">
+            SalesItem
+        </div>
+    )
+}
+
+export default SalesItem
